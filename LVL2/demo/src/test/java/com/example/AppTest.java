36b734add92a3d2b3a6e4c6c0e4bb400cb3337e5
jorbort <jorbort92@gmail.com>
2025-03-24 12:11:19 +0100

    REFACTOR: refacotrizado la estrucctura de carpetas para usar maven

--- LVL2/demo/src/test/java/com/example/AppTest.java
+++ LVL2/demo/src/test/java/com/example/AppTest.java
@@ -2,7 +2,7 @@ package com.example;
 
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.HashMap;
